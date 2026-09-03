fbb750a9c7a052e5844587e5e3caa8a263a12f02
junayedsheikh457-lang <junayedsheikh457@gmail.com>
2026-09-04 02:15:12 +0600

    Enable fashion backend routes and database

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import os
 from app import app, init_db, setup_webhook, PORT
+import fashion_backend
 
 if __name__ == '__main__':
     init_db()
+    fashion_backend.init_fashion_db()
     setup_webhook()
     app.run(host='0.0.0.0', port=PORT)
