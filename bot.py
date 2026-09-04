a62b367dd351003bcd258b1215c062238c6a4cbb
junayedsheikh457-lang <junayedsheikh457@gmail.com>
2026-09-04 17:06:38 +0600

    Seed starter fashion catalog on deployment

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import os
 from app import app, init_db, PORT
+from fashion.seed_products import seed_products
 
 if __name__ == '__main__':
     init_db()
+    seed_products()
     app.run(host='0.0.0.0', port=PORT)
