925e5138b0f6fa39111f9bf27055ccffe7b1a1a7
junayedsheikh457-lang <junayedsheikh457@gmail.com>
2026-09-05 02:38:00 +0600

    Run product gallery seeder on startup

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import os
 from app import app, init_db, PORT
 from fashion.seed_products import seed_products
+from fashion.gallery_seed import seed_galleries
 
 if __name__ == '__main__':
     init_db()
     seed_products()
+    seed_galleries()
     app.run(host='0.0.0.0', port=PORT)
